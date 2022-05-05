01def7880fca347036964e96f5892ddb5d773885
pechoutmatej <73175139+pechoutmatej@users.noreply.github.com>
2022-05-05 13:13:59 +0200

    update tests

--- server_test.go
+++ server_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -29,13 +30,34 @@ func Test_echo(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			echo(tt.args.w, tt.args.r)
 		})
+	}
+}
+func Test_echoEur(t *testing.T) {
+	got := echoEur()
+	want := "Actual price of one € is "
+	if !strings.HasPrefix(got, want) {
+		t.Errorf("echoEur() = %v, want %v", got, want)
+	}
+}
+
+func Test_echoName(t *testing.T) {
+	got := echoName()
+	want := "Chatbots name is "
+	if !strings.HasPrefix(got, want) {
+		t.Errorf("echoName() = %v, want %v", got, want)
+	}
+}
+
+func Test_echoTime(t *testing.T) {
+	got := echoTime()
+	want := "Actual server time is "
+	if !strings.HasPrefix(got, want) {
+		t.Errorf("echoTime() = %v, want %v", got, want)
 	}
 }
 
